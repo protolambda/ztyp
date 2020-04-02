4ef050b9effc4a45619cae4637240a0a3681652d
protolambda <proto@protolambda.com>
2020-04-02 14:58:16 +0200

    basic types (uints/bool), basic views, small-byte-vec type/view

--- view/basic.go
+++ view/basic.go
@@ -2,22 +2,36 @@ package view
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	. "github.com/protolambda/ztyp/tree"
+	"io"
 )
 
 // A uint type, identified by its size in bytes.
 type UintMeta uint64
 
+func (td UintMeta) Default(_ BackingHook) View {
+	switch td {
+	case Uint8Type:
+		return Uint8View(0)
+	case Uint16Type:
+		return Uint16View(0)
+	case Uint32Type:
+		return Uint32View(0)
+	case Uint64Type:
+		return Uint64View(0)
+	default:
+		// unsupported uint type
+		return nil
+	}
+}
+
 func (td UintMeta) DefaultNode() Node {
 	return &ZeroHashes[0]
 }
 
-func (td UintMeta) ByteLength() uint64 {
-	return uint64(td)
-}
-
-func (td UintMeta) ViewFromBacking(node Node, _ ViewHook) (View, error) {
+func (td UintMeta) ViewFromBacking(node Node, _ BackingHook) (View, error) {
 	v, ok := node.(*Root)
 	if !ok {
 		return nil, fmt.Errorf("node %v must be a root to read a uint%d from it", node, td)
@@ -32,12 +46,12 @@ func (td UintMeta) ViewFromBacking(node Node, _ ViewHook) (View, error) {
 	case Uint64Type:
 		return Uint64View(binary.LittleEndian.Uint64(v[:8])), nil
 	default:
-		// unsupported backing
-		return nil, fmt.Errorf("uint%d viewing is not supported", td)
+		// unsupported uint type
+		return nil, fmt.Errorf("uint%d viewing is not supported", td*8)
 	}
 }
 
-func (td UintMeta) SubViewFromBacking(v *Root, i uint8) (SubView, error) {
+func (td UintMeta) SubViewFromBacking(v *Root, i uint8) (BasicView, error) {
 	if uint64(i) >= (32 / uint64(td)) {
 		return nil, fmt.Errorf("cannot get subview %d for uint%d type", i, td)
 	}
@@ -56,6 +70,35 @@ func (td UintMeta) SubViewFromBacking(v *Root, i uint8) (SubView, error) {
 	}
 }
 
+func (td UintMeta) IsFixedByteLength() bool {
+	return true
+}
+
+func (td UintMeta) TypeByteLength() uint64 {
+	return uint64(td)
+}
+
+func (td UintMeta) MinByteLength() uint64 {
+	return uint64(td)
+}
+
+func (td UintMeta) MaxByteLength() uint64 {
+	return uint64(td)
+}
+
+func (td UintMeta) Deserialize(r io.Reader, scope uint64) error {
+	// TODO
+	return nil
+}
+
+func (td UintMeta) Name() string {
+	return fmt.Sprintf("uint%d", td*8)
+}
+
+func (td UintMeta) String() string {
+	return td.Name()
+}
+
 const (
 	Uint8Type  UintMeta = 1
 	Uint16Type UintMeta = 2
@@ -63,7 +106,13 @@ const (
 	Uint64Type UintMeta = 8
 )
 
+var BasicViewNoSetBackingError = errors.New("basic views cannot set new backing")
+
 type Uint8View uint8
+
+func (v Uint8View) SetBacking(b Node) error {
+	return BasicViewNoSetBackingError
+}
 
 func (v Uint8View) Backing() Node {
 	out := &Root{}
@@ -80,6 +129,29 @@ func (v Uint8View) BackingFromBase(base *Root, i uint8) *Root {
 	return &newRoot
 }
 
+func (v Uint8View) Copy() (View, error) {
+	return v, nil
+}
+
+func (v Uint8View) ValueByteLength() (uint64, error) {
+	return 1, nil
+}
+
+func (v Uint8View) Serialize(w io.Writer) error {
+	_, err := w.Write([]byte{byte(v)})
+	return err
+}
+
+func (v Uint8View) HashTreeRoot(h HashFn) Root {
+	newRoot := Root{}
+	newRoot[0] = uint8(v)
+	return newRoot
+}
+
+func (v Uint8View) Type() TypeDef {
+	return Uint8Type
+}
+
 // Alias to Uint8Type
 const ByteType = Uint8Type
 
@@ -87,6 +159,10 @@ const ByteType = Uint8Type
 type ByteView = Uint8View
 
 type Uint16View uint16
+
+func (v Uint16View) SetBacking(b Node) error {
+	return BasicViewNoSetBackingError
+}
 
 func (v Uint16View) Backing() Node {
 	out := &Root{}
@@ -99,11 +175,38 @@ func (v Uint16View) BackingFromBase(base *Root, i uint8) *Root {
 		return nil
 	}
 	newRoot := *base
-	binary.LittleEndian.PutUint16(newRoot[i*2:i*2+2], uint16(v))
+	binary.LittleEndian.PutUint16(newRoot[i<<1:(i<<1)+2], uint16(v))
 	return &newRoot
 }
 
+func (v Uint16View) Copy() (View, error) {
+	return v, nil
+}
+
+func (v Uint16View) ValueByteLength() (uint64, error) {
+	return 2, nil
+}
+
+func (v Uint16View) Serialize(w io.Writer) error {
+	_, err := w.Write([]byte{byte(v), byte(v >> 8)})
+	return err
+}
+
+func (v Uint16View) HashTreeRoot(h HashFn) Root {
+	newRoot := Root{}
+	binary.LittleEndian.PutUint16(newRoot[:], uint16(v))
+	return newRoot
+}
+
+func (v Uint16View) Type() TypeDef {
+	return Uint16Type
+}
+
 type Uint32View uint32
+
+func (v Uint32View) SetBacking(b Node) error {
+	return BasicViewNoSetBackingError
+}
 
 func (v Uint32View) Backing() Node {
 	out := &Root{}
@@ -120,7 +223,34 @@ func (v Uint32View) BackingFromBase(base *Root, i uint8) *Root {
 	return &newRoot
 }
 
+func (v Uint32View) Copy() (View, error) {
+	return v, nil
+}
+
+func (v Uint32View) ValueByteLength() (uint64, error) {
+	return 4, nil
+}
+
+func (v Uint32View) Serialize(w io.Writer) error {
+	_, err := w.Write([]byte{byte(v), byte(v >> 8), byte(v >> 16), byte(v >> 24)})
+	return err
+}
+
+func (v Uint32View) HashTreeRoot(h HashFn) Root {
+	newRoot := Root{}
+	binary.LittleEndian.PutUint32(newRoot[:], uint32(v))
+	return newRoot
+}
+
+func (v Uint32View) Type() TypeDef {
+	return Uint32Type
+}
+
 type Uint64View uint64
+
+func (v Uint64View) SetBacking(b Node) error {
+	return BasicViewNoSetBackingError
+}
 
 func (v Uint64View) Backing() Node {
 	out := &Root{}
@@ -137,9 +267,34 @@ func (v Uint64View) BackingFromBase(base *Root, i uint8) *Root {
 	return &newRoot
 }
 
+func (v Uint64View) Copy() (View, error) {
+	return v, nil
+}
+
+func (v Uint64View) ValueByteLength() (uint64, error) {
+	return 8, nil
+}
+
+func (v Uint64View) Serialize(w io.Writer) error {
+	b := [8]byte{}
+	binary.LittleEndian.PutUint64(b[:], uint64(v))
+	_, err := w.Write(b[:])
+	return err
+}
+
+func (v Uint64View) HashTreeRoot(h HashFn) Root {
+	newRoot := Root{}
+	binary.LittleEndian.PutUint64(newRoot[:], uint64(v))
+	return newRoot
+}
+
+func (v Uint64View) Type() TypeDef {
+	return Uint64Type
+}
+
 type BoolMeta uint8
 
-func (td BoolMeta) SubViewFromBacking(v *Root, i uint8) SubView {
+func (td BoolMeta) SubViewFromBacking(v *Root, i uint8) BasicView {
 	if i >= 32 {
 		return nil
 	}
@@ -156,15 +311,15 @@ func (td BoolMeta) BoolViewFromBitfieldBacking(v *Root, i uint8) (BoolView, erro
 	return (v[i>>3]>>(i&7))&1 == 1, nil
 }
 
+func (td BoolMeta) Default(_ BackingHook) View {
+	return BoolView(false)
+}
+
 func (td BoolMeta) DefaultNode() Node {
 	return &ZeroHashes[0]
 }
 
-func (td BoolMeta) ByteLength() uint64 {
-	return 1
-}
-
-func (td BoolMeta) ViewFromBacking(node Node, _ ViewHook) (View, error) {
+func (td BoolMeta) ViewFromBacking(node Node, _ BackingHook) (View, error) {
 	v, ok := node.(*Root)
 	if !ok {
 		return nil, fmt.Errorf("node %v must be a root to read a bool from it", node)
@@ -172,11 +327,44 @@ func (td BoolMeta) ViewFromBacking(node Node, _ ViewHook) (View, error) {
 	return BoolView(v[0] != 0), nil
 }
 
+func (td BoolMeta) IsFixedByteLength() bool {
+	return true
+}
+
+func (td BoolMeta) TypeByteLength() uint64 {
+	return 1
+}
+
+func (td BoolMeta) MinByteLength() uint64 {
+	return 1
+}
+
+func (td BoolMeta) MaxByteLength() uint64 {
+	return 1
+}
+
+func (td BoolMeta) Deserialize(r io.Reader, scope uint64) error {
+	// TODO
+	return nil
+}
+
+func (td BoolMeta) Name() string {
+	return "bool"
+}
+
+func (td BoolMeta) String() string {
+	return "bool"
+}
+
 const BoolType BoolMeta = 0
 
 type BoolView bool
 
 var trueRoot = &Root{1}
+
+func (v BoolView) SetBacking(b Node) error {
+	return BasicViewNoSetBackingError
+}
 
 func (v BoolView) Backing() Node {
 	if v {
@@ -196,6 +384,14 @@ func (v BoolView) BackingFromBitfieldBase(base *Root, i uint8) *Root {
 	return &newRoot
 }
 
+func (v BoolView) byte() byte {
+	if v {
+		return 1
+	} else {
+		return 0
+	}
+}
+
 func (v BoolView) BackingFromBase(base *Root, i uint8) *Root {
 	if i >= 32 {
 		return nil
@@ -207,4 +403,27 @@ func (v BoolView) BackingFromBase(base *Root, i uint8) *Root {
 		newRoot[i] = 0
 	}
 	return &newRoot
+}
+
+func (v BoolView) Copy() (View, error) {
+	return v, nil
+}
+
+func (v BoolView) ValueByteLength() (uint64, error) {
+	return 1, nil
+}
+
+func (v BoolView) Serialize(w io.Writer) error {
+	_, err := w.Write([]byte{v.byte()})
+	return err
+}
+
+func (v BoolView) HashTreeRoot(h HashFn) Root {
+	newRoot := Root{}
+	newRoot[0] = v.byte()
+	return newRoot
+}
+
+func (v BoolView) Type() TypeDef {
+	return BoolType
 }
--- view/small_byte_vec.go
+++ view/small_byte_vec.go
@@ -1,18 +1,26 @@
 package view
 
 import (
+	"errors"
 	"fmt"
 	. "github.com/protolambda/ztyp/tree"
+	"io"
 )
 
 // To represent views of < 32 bytes efficiently as just a slice of those bytes.
+// Values above 32 are invalid.
+// For 32, using Root ([32]byte as underlying type) is better.
 type SmallByteVecMeta uint8
+
+func (td SmallByteVecMeta) Default(_ BackingHook) View {
+	return make(SmallByteVecView, td, td)
+}
 
 func (td SmallByteVecMeta) DefaultNode() Node {
 	return &ZeroHashes[0]
 }
 
-func (td SmallByteVecMeta) ViewFromBacking(node Node, _ ViewHook) (View, error) {
+func (td SmallByteVecMeta) ViewFromBacking(node Node, _ BackingHook) (View, error) {
 	r, ok := node.(*Root)
 	if !ok {
 		return nil, fmt.Errorf("backing must be a root")
@@ -25,7 +33,40 @@ func (td SmallByteVecMeta) ViewFromBacking(node Node, _ ViewHook) (View, error) 
 	return v, nil
 }
 
+func (td SmallByteVecMeta) IsFixedByteLength() bool {
+	return true
+}
+
+func (td SmallByteVecMeta) TypeByteLength() uint64 {
+	return uint64(td)
+}
+
+func (td SmallByteVecMeta) MinByteLength() uint64 {
+	return uint64(td)
+}
+
+func (td SmallByteVecMeta) MaxByteLength() uint64 {
+	return uint64(td)
+}
+
+func (td SmallByteVecMeta) Deserialize(r io.Reader, scope uint64) error {
+	// TODO
+	return nil
+}
+
+func (td SmallByteVecMeta) Name() string {
+	return fmt.Sprintf("Vector[byte, %d]", td)
+}
+
+func (td SmallByteVecMeta) String() string {
+	return td.Name()
+}
+
 type SmallByteVecView []byte
+
+func (v SmallByteVecView) SetBacking(b Node) error {
+	return errors.New("cannot set backing of SmallByteVecView")
+}
 
 func (v SmallByteVecView) Backing() Node {
 	out := &Root{}
@@ -33,4 +74,29 @@ func (v SmallByteVecView) Backing() Node {
 	return out
 }
 
-const Bytes4 SmallByteVecMeta = 4
+func (v SmallByteVecView) Copy() (View, error) {
+	return v, nil
+}
+
+func (v SmallByteVecView) ValueByteLength() (uint64, error) {
+	return uint64(len(v)), nil
+}
+
+func (v SmallByteVecView) Serialize(w io.Writer) error {
+	_, err := w.Write(v)
+	return err
+}
+
+func (v SmallByteVecView) HashTreeRoot(h HashFn) Root {
+	newRoot := Root{}
+	copy(newRoot[:], v)
+	return newRoot
+}
+
+func (v SmallByteVecView) Type() TypeDef {
+	return SmallByteVecMeta(len(v))
+}
+
+const Bytes4Type SmallByteVecMeta = 4
+const Bytes8Type SmallByteVecMeta = 8
+const Bytes16Type SmallByteVecMeta = 16
