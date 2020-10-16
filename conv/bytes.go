8d249a29f3ae27cb3627b97545db19c404bf588f
protolambda <proto@protolambda.com>
2020-10-16 22:38:24 +0200

    marshal/unmarshal helpers

---
+++ conv/bytes.go
@@ -0,0 +1,29 @@
+package conv
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+func BytesMarshalText(b []byte) ([]byte, error) {
+	l := 2 + (len(b) * 2)
+	res := make([]byte, l, l)
+	res[0] = '0'
+	res[1] = 'x'
+	hex.Encode(res[2:], b)
+	return res, nil
+}
+
+func FixedBytesUnmarshalText(dst []byte, text []byte) error {
+	if dst == nil {
+		return DestNilErr
+	}
+	if len(text) >= 2 && text[0] == '0' && (text[1] == 'x' || text[1] == 'X') {
+		text = text[2:]
+	}
+	if len(text) != 2*len(dst) {
+		return fmt.Errorf("unexpected length %d, expected %d, got string '%s'", len(text), len(dst), string(text))
+	}
+	_, err := hex.Decode(dst, text)
+	return err
+}
--- view/root.go
+++ view/root.go
@@ -3,6 +3,7 @@ package view
 import (
 	"fmt"
 	"github.com/protolambda/ztyp/codec"
+	"github.com/protolambda/ztyp/conv"
 	. "github.com/protolambda/ztyp/tree"
 )
 
@@ -102,4 +103,12 @@ func (r *RootView) Serialize(w *codec.EncodingWriter) error {
 
 func (r *RootView) HashTreeRoot(h HashFn) Root {
 	return Root(*r)
+}
+
+func (r *RootView) MarshalText(b []byte) ([]byte, error) {
+	return conv.BytesMarshalText(b)
+}
+
+func (r *RootView) UnmarshalText(text []byte) error {
+	return conv.FixedBytesUnmarshalText(r[:], text)
 }
